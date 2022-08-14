b10456a4faf7b2326710f481bfca5fd3ca2db23d
unknown <harshit.sri2211@gmail.com>
2022-08-14 19:15:23 +0530

    added swagger

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+
+
 require('dotenv').config({path:"./config.env"})
 const orderRouter = require('./router/orderRouter')
+const swaggerJSDoc = require('swagger-jsdoc')
 require('./db/connection')
 
 const port = process.env.PORT || 8000
@@ -11,7 +16,19 @@ const app = express()
 app.use(express.json())
 app.use(orderRouter)
 
+const swaggerOptions = {
+    swaggerDefinition :{
+        info:{
+            'title':'Milk Order Management',
+            'version':'0.1'
+        }
+    },
+    apis:['./router/orderRouter.js']
+}
 
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+
+app.use('/api-doc',swaggerUi.serve,swaggerUi.setup(swaggerDocs))
 app.listen(port,()=>{
     console.log(`listening on ${port}`)
 })
--- router/orderRouter.js
+++ router/orderRouter.js
@@ -1,12 +1,95 @@
 const express = require('express')
 const { getAllOrders, addOrder, updateOrder, updateStatus, deleteOrder, checkCapacity } = require('../controller/orderController')
 const router = express.Router()
+/**
+ * @swagger
+ * /getOrders:
+ *  get:
+ *      description: Get all Orders
+ *      responses : 
+ *          200:
+ *              description: Success
+ */
 
 router.get('/getOrders',getAllOrders)
+
+/**
+ * @swagger
+ * /add:
+ *  post:
+ *      description: Add a Order
+ *      parameters:
+ *          -name: Name
+ *           required:true
+ *          
+ *  
+ *      responses : 
+ *          201:
+ *              description: Created
+ *          400:
+ *              description: Order already exist
+ *          
+ */
 router.post('/add',addOrder)
+/**
+ * @swagger
+ * /update/{id}:
+ *  patch:
+ *      description: Update a Order
+ *      parameters:
+ *          -name: Name
+ *           required:true
+ *          
+ *  
+ *      responses : 
+ *          200:
+ *              description: Updated
+ *          400:
+ *              description: Any Error
+ *          
+ */
 router.patch('/update/:id',updateOrder)
+/**
+ * @swagger
+ * /updateStatus/{id}:
+ *  patch:
+ *      description: Update a Order Status
+ *   
+ *      responses : 
+ *          200:
+ *              description: Updated
+ *          400:
+ *              description: Any Error
+ *          
+ */
 router.patch('/updateStatus/:id',updateStatus)
+/**
+ * @swagger
+ * /delete/{id}:
+ *  get:
+ *      description: DELETE a Order
+ *   
+ *      responses : 
+ *          200:
+ *              description: Updated
+ *          400:
+ *              description: Any Error
+ *          
+ */
 router.get('/delete/:id',deleteOrder)
+/**
+ * @swagger
+ * /checkCapacity/{date}:
+ *  get:
+ *      description: Check Capacity left for given date(D-M-Y)
+ *   
+ *      responses : 
+ *          200:
+ *              description: Updated
+ *          400:
+ *              description: Any Error
+ *          
+ */
 router.get('/checkCapacity/:date',checkCapacity)
 
 module.exports = router
